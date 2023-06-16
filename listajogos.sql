f77983292de995160acb60f8d63d491403a65c64
JaxckSkelet <136658772+JaxckSkelet@users.noreply.github.com>
2023-06-16 13:16:41 -0300

    Create listajogos.sql

---
+++ listajogos.sql
@@ -0,0 +1,6 @@
+SELECT partida_id, time_casa, time_fora, data_jogo, estadio, valor_ingresso
+FROM partidas
+WHERE data_jogo > CURDATE();
+
+
+# codigo para obter a lista de todos os jogos que ainda estão disponiveis
