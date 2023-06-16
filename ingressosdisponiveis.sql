b8c9a720c012e4eed97c3a72cacef6dd051574fd
JaxckSkelet <136658772+JaxckSkelet@users.noreply.github.com>
2023-06-16 14:32:25 -0300

    Create ingressosdisponiveis.sql

---
+++ ingressosdisponiveis.sql
@@ -0,0 +1,7 @@
+SELECT ingressos.ingresso_id, partidas.time_casa, partidas.time_fora, partidas.data_jogo, partidas.estadio, ingressos.setor, ingressos.preço
+FROM ingressos
+INNER JOIN partidas ON ingressos.partida_id = partidas.partida_id
+WHERE partidas.time_casa = 'Nome do Time da Casa'
+  AND partidas.time_fora = 'Nome do Time Visitante'
+  AND partidas.data_jogo = 'YYYY-MM-DD'
+  AND partidas.estadio = 'Nome do Estádio';
