ace582320515e333b26242198d06e2022fa3aadd
JaxckSkelet <136658772+JaxckSkelet@users.noreply.github.com>
2023-06-16 14:47:54 -0300

    Update ingressosdisponiveis.sql

--- ingressosdisponiveis.sql
+++ ingressosdisponiveis.sql
@@ -1,4 +1,4 @@
-SELECT ingressos.ingresso_id, partidas.time_casa, partidas.time_fora, partidas.data_jogo, partidas.estadio, ingressos.setor, ingressos.preço
+SELECT ingressos.ingresso_id, partidas.time_casa, partidas.time_fora, partidas.data_jogo, partidas.estadio, ingressos.setor, ingressos.valor
 FROM ingressos
 INNER JOIN partidas ON ingressos.partida_id = partidas.partida_id
 WHERE partidas.time_casa = 'Nome do Time da Casa'
