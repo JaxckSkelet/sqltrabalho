fc7484cbc4e4b1402682c2d55de5690b4efe2dba
JaxckSkelet <136658772+JaxckSkelet@users.noreply.github.com>
2023-06-17 00:16:43 -0300

    Update hc.sql

--- hc.sql
+++ hc.sql
@@ -1,6 +1,6 @@
 SELECT hc.compra_id, p.time_casa, p.time_fora, p.data_jogo, p.estadio, hc.quantidade_ingressos, hc.valor_total, hc.data_compra
 FROM historico_compras hc
 INNER JOIN partidas p ON hc.partida_id = p.partida_id
-WHERE hc.user_id = <1>;
+WHERE hc.user_id = <1>; --> o numero 1 se trata do id do usuario
 
 #mostra o historico de compras 
